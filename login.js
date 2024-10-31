10b6650905f49393151abc92eeae981eb75b947f
ChuangB48 <kecianchuang@gmail.com>
2024-10-31 22:07:38 +0800

    add firework

--- login.js
+++ login.js
@@ -8,3 +8,24 @@ function start(){
         location.href="message.html";
     }
 }
+window.addEventListener("click",function(mouse){
+    let mousey=mouse.clientX-25;
+    let mousex=mouse.clientY-25;
+    document.getElementById("fireboard").style.top=mousex.toString()+"px";
+    document.getElementById("fireboard").style.left=mousey.toString()+"px";
+    document.getElementById("firework").style.opacity="1";
+    let o=1.0,s=5.0;
+    let time=window.setInterval(function(){
+        document.getElementById("firework").style.opacity=o.toString();
+        document.getElementById("firework").style.width=s.toString()+"px";
+        document.getElementById("firework").style.height=s.toString()+"px";
+        o-=0.02;
+        s+=1;
+        if(o<=0){
+            window.clearInterval(time);
+        }
+    },10);
+    document.getElementById("firework").style.opacity="0";
+    document.getElementById("firework").style.width="5px";
+    document.getElementById("firework").style.height="5px";
+});
